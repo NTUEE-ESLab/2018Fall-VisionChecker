0367d8b6a56dac0588a2e0d3459c7b8f1383ea31
TeresaChou <chowduen@gmail.com>
2019-01-11 05:23:50 +0000

    .

--- button.py
+++ button.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-   inputValue = GPIO.input(11)
-   if inputValue == True:
-      print("Button pressed.")
-      while inputValue == True:
+   inputValue = GPIO.input(18)
+   if inputValue == False:
+       print(Button pressed)
+      while inputValue == False:
          time.sleep(0.3)
